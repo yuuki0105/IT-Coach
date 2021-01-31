5adbaadf046dfeb23f5722a71aef7990de71a221
github yuuki0105 <komatsu3613@gmail.com>
2021-01-31 22:02:12 +0900

    mypage-form fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks",registrations: "users/registrations" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :users, only: [:show]
   resources :coaches, only: [:show]
+  resource :mypage, only: [:edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
