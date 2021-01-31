2d898c390706297a449d095595ebff69c127f25c
github yuuki0105 <komatsu3613@gmail.com>
2021-01-31 22:36:25 +0900

    create setting_email

---
+++ app/controllers/settings/emails_controller.rb
@@ -0,0 +1,21 @@
+class Settings::EmailsController < ApplicationController
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to setting_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+end
