d7dca025483400c0bac68ff496d95ab3b6f8b01a
github yuuki0105 <komatsu3613@gmail.com>
2021-02-03 12:06:59 +0900

    plan form & link create

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -8,24 +8,28 @@ class PlansController < ApplicationController
   def create
     @plan = Plan.new(plan_params)
     if @plan.save
-      redirect_to coach_path
-      #ここのリダイレクト先は後から変更予定
+      redirect_to coach_path(@plan.coach)
     else
       render :new
     end
   end
 
   def edit
-
+    @plan = current_user.coach.plans.find(params[:id])
   end
 
   def update
-
+    @plan = current_user.coach.plans.find(params[:id])
+    if @plan.update(plan_params)
+      redirect_to coach_path(@plan.coach)
+    else
+      render :new
+    end
   end
 
   private
   def plan_params
-    params.require(:plan).permit(:title).merge(coach_id: params[:coach_id])
+    params.require(:plan).permit(:title, :term, :communication_style, :fee, :target, :content, :published).merge(coach_id: current_user.coach.id)
   end
 
 end
