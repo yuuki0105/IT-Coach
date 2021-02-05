b420a1f765fe5e3fb6cfcd5a9b00ed6b098d1ec7
github yuuki0105 <komatsu3613@gmail.com>
2021-02-06 00:18:24 +0900

    create-career-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_134704) do
+ActiveRecord::Schema.define(version: 2021_02_05_144320) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,18 @@ ActiveRecord::Schema.define(version: 2021_02_01_134704) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "careers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "coach_id", null: false
+    t.string "organization", null: false
+    t.string "role"
+    t.date "start_month", null: false
+    t.date "end_month"
+    t.boolean "ongoing", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["coach_id"], name: "index_careers_on_coach_id"
   end
 
   create_table "coaches", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -123,6 +135,7 @@ ActiveRecord::Schema.define(version: 2021_02_01_134704) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "careers", "coaches"
   add_foreign_key "coaches", "users"
   add_foreign_key "plans", "coaches"
   add_foreign_key "skills", "skill_categories"
---
+++ spec/models/career_spec.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: careers
+#
+#  id           :bigint           not null, primary key
+#  end_month    :date
+#  ongoing      :boolean          default(FALSE), not null
+#  organization :string(255)      not null
+#  role         :string(255)
+#  start_month  :date             not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  coach_id     :bigint           not null
+#
+# Indexes
+#
+#  index_careers_on_coach_id  (coach_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (coach_id => coaches.id)
+#
+require 'rails_helper'
+
+RSpec.describe Career, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
