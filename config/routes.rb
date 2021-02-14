54f27c54f4722ce4783944fb03f4e6a9cccff8ae
github yuuki0105 <komatsu3613@gmail.com>
2021-02-14 23:43:07 +0900

    messagesの不要ファイル削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :users, only: [:show] do
-    resources :messages, only: [:index, :create, :show]
     resources :rooms, only: [:create], module: :users
   end
 
