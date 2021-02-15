084ed32f517affd98d979e56604d79a9de41a2e6
github yuuki0105 <komatsu3613@gmail.com>
2021-02-16 00:51:12 +0900

    exiamination_statusのIDを定数で定義した

--- app/models/examination_status.rb
+++ app/models/examination_status.rb
@@ -3,4 +3,9 @@ class ExaminationStatus < ActiveYaml::Base
   set_root_path "config/divisions"
   set_filename "examination_status"
   enum_accessor :type
+
+  BEFORE_ID = 1
+  PASSED_ID = 10
+  FAILED_ID = 20
+
 end
