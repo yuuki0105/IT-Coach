16b7bf639b5b5813ebefa34a42110d8029579119
github yuuki0105 <komatsu3613@gmail.com>
2021-02-21 00:24:34 +0900

    キーワード検索機能をつくる

--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,8 @@ Rails.application.routes.draw do
     resource :email, only: [:edit, :update], module: :settings
   end
 
+  resources :search, only: [:index] do
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
