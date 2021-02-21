3d62fd464075fd2cda5a7653a96f800b3b2caf91
github yuuki0105 <komatsu3613@gmail.com>
2021-02-21 23:21:52 +0900

    カテゴリー検索のコーチ一覧の表示

--- app/controllers/searches/skill_categories/skills_controller.rb
+++ app/controllers/searches/skill_categories/skills_controller.rb
@@ -3,11 +3,17 @@ class Searches::SkillCategories::SkillsController < ApplicationController
   def index
     @skill_category = SkillCategory.find_by(en_name: params[:skill_category_en_name])
     @skills = @skill_category.skills
+
+    user_ids = User.eager_load(:user_skills).where(user_skills: {id: @skills.pluck(:id)}).pluck(:id)
+    @coaches = Coach.where(user_id: user_ids)
   end
 
   def show
     @skill_category = SkillCategory.find_by(en_name: params[:skill_category_en_name])
     @skill = @skill_category.skills.find_by(en_name: params[:en_name])
+
+    user_ids = @skill.users.pluck(:id)
+    @coaches = Coach.where(user_id: user_ids)
   end
 
 end
--- app/controllers/searches/skill_categories_controller.rb
+++ app/controllers/searches/skill_categories_controller.rb
@@ -2,6 +2,7 @@ class Searches::SkillCategoriesController < ApplicationController
 
   def index
     @skill_categories = SkillCategory.all
+    @coaches = Coach.all
   end
 
 end
