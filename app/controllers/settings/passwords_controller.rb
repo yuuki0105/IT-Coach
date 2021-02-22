24bf2af651383518fcb1d7701a19cd7cadc82957
github yuuki0105 <komatsu3613@gmail.com>
2021-02-23 01:25:39 +0900

    パスワード更新の処理

--- app/controllers/settings/passwords_controller.rb
+++ app/controllers/settings/passwords_controller.rb
@@ -4,4 +4,40 @@ class Settings::PasswordsController < ApplicationController
     @user = current_user
   end
 
+  def update
+    @user = current_user
+
+    if user_params[:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    end
+
+    if @user.valid_password?(current_user_params[:current_password])
+      #パスワード変更
+      if @user.update(user_params)
+        #ログインし直すことができるようにしたい
+        session[:user_id] = @user.id
+        flash[:success] = "パスワードを更新できました"
+        redirect_to setting_path
+      else
+        render 'edit'
+      end
+    else
+      #元の画面に戻す（エラーメッセージを正しく直す）
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    end
+
+  end
+
+  private
+
+    def current_user_params
+      params.require(:user).permit(:current_password)
+    end
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
 end
