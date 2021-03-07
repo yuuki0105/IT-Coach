05ea34583838f102bc60496ad0d59b334d9653c8
github yuuki0105 <komatsu3613@gmail.com>
2021-03-07 23:58:29 +0900

    審査面談日程のcontroller,view作成

--- db/migrate/20210125152040_create_coaches.rb
+++ db/migrate/20210125152040_create_coaches.rb
@@ -3,7 +3,7 @@ class CreateCoaches < ActiveRecord::Migration[6.1]
     create_table :coaches do |t|
 
       t.references :user, null: false, foreign_key: true
-      t.boolean :schedule_confirmed, null: false, default: false
+      t.boolean :examination_interview_date_confirmed, null: false, default: false, comment: '審査面談日程の確定'
       t.integer :examination_status_id, null: false, default: 1
       t.date :examination_result_date, comment: '審査結果日'
 
--- spec/factories/coaches.rb
+++ spec/factories/coaches.rb
@@ -2,13 +2,13 @@
 #
 # Table name: coaches
 #
-#  id                                  :bigint           not null, primary key
-#  user_id                             :bigint           not null
-#  schedule_confirmed                  :boolean          default(FALSE), not null
-#  examination_status_id               :integer          default(1), not null
-#  examination_result_date(審査結果日) :date
-#  created_at                          :datetime         not null
-#  updated_at                          :datetime         not null
+#  id                                                       :bigint           not null, primary key
+#  user_id                                                  :bigint           not null
+#  examination_interview_date_confirmed(審査面談日程の確定) :boolean          default(FALSE), not null
+#  examination_status_id                                    :integer          default(1), not null
+#  examination_result_date(審査結果日)                      :date
+#  created_at                                               :datetime         not null
+#  updated_at                                               :datetime         not null
 #
 # Indexes
 #
