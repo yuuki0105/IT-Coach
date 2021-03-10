3df9c0c49a44887b0e389d7c3b07e03e1706989d
github yuuki0105 <komatsu3613@gmail.com>
2021-03-10 12:20:44 +0900

    管理画面の作成

---
+++ app/admin/coaches.rb
@@ -0,0 +1,11 @@
+ActiveAdmin.register Coach do
+
+  permit_params :examination_interview_date_confirmed, :examination_status_id, :examination_result_date
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+
+end
