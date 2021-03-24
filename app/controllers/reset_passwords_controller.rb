a2af7cc88a17d7b6db51e1dc3278b410d9542d7b
github yuuki0105 <komatsu3613@gmail.com>
2021-03-24 12:36:19 +0900

    パスワード更新（リンク追加、alertの修正）

--- app/controllers/reset_passwords_controller.rb
+++ app/controllers/reset_passwords_controller.rb
@@ -14,13 +14,13 @@ class ResetPasswordsController < ApplicationController
   def edit
     @user =User.find_by(reset_password_token: params[:token])
     unless @user&.reset_password_period_valid?
-      redirect_to reset_password_path, error: '失敗しました'
+      redirect_to reset_password_path, alert: '失敗しました'
     end
   end
 
   def update
     @user = User.find_by(reset_password_token: user_params[:reset_password_token])
-    redirect_to reset_password_path, error: '失敗しました' unless @user&.reset_password_period_valid?
+    redirect_to reset_password_path, alert: '失敗しました' unless @user&.reset_password_period_valid?
     @user.reset_password(user_params[:password], user_params[:password_confirmation])
     redirect_to new_user_session_path, notice: 'パスワードを更新しました'
   end
