69d4f64b8431293b012a984cc902814f1e64baac
github yuuki0105 <komatsu3613@gmail.com>
2021-03-24 12:25:01 +0900

    パスワード変更のedit、updateづくり

--- app/controllers/reset_passwords_controller.rb
+++ app/controllers/reset_passwords_controller.rb
@@ -1,20 +1,33 @@
 class ResetPasswordsController < ApplicationController
 
-def show
-end
+  def show
+  end
 
-def create
-  user = User.find_by(email: params[:email])
-  token = SecureRandom.urlsafe_base64(40)
-  user.update(reset_password_token: token, reset_password_sent_at: Time.current)
-  User::Mailer.reset_password_email(user).deliver_now
+  def create
+    user = User.find_by(email: params[:email])
+    token = SecureRandom.urlsafe_base64(40)
+    user.update(reset_password_token: token, reset_password_sent_at: Time.current)
+    User::Mailer.reset_password_email(user).deliver_now
+    redirect_to reset_password_path, notice: 'メールを送信しました'
+  end
 
-  redirect_to reset_password_path, notice: 'メールを送信しました'
+  def edit
+    @user =User.find_by(reset_password_token: params[:token])
+    unless @user&.reset_password_period_valid?
+      redirect_to reset_password_path, error: '失敗しました'
+    end
+  end
 
-end
+  def update
+    @user = User.find_by(reset_password_token: user_params[:reset_password_token])
+    redirect_to reset_password_path, error: '失敗しました' unless @user&.reset_password_period_valid?
+    @user.reset_password(user_params[:password], user_params[:password_confirmation])
+    redirect_to new_user_session_path, notice: 'パスワードを更新しました'
+  end
 
-def update
-
-end
+  private
+  def user_params
+    params.require(:user).permit(:reset_password_token, :password, :password_confirmation)
+  end
 
 end
