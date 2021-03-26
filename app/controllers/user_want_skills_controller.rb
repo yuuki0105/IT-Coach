c12a35fb0a73bb5512e2722e7147c9df11dc04d7
github yuuki0105 <komatsu3613@gmail.com>
2021-03-26 23:52:58 +0900

    user_want_skillsのエラーハンドリング追加

--- app/controllers/user_want_skills_controller.rb
+++ app/controllers/user_want_skills_controller.rb
@@ -14,11 +14,15 @@ class UserWantSkillsController < ApplicationController
   def create
     skill_ids = user_want_skill_params[:want_skills].reject { |skill_id| skill_id.blank? }.map{ |skill_id| skill_id.to_i }
     user_want_skills = []
-    skill_ids.each do |skill_id|
-      user_want_skills << UserWantSkill.new(user_id: current_user.id, skill_id: skill_id)
+    ActiveRecord::Base.transaction do
+      skill_ids.each do |skill_id|
+        user_want_skills << UserWantSkill.new(user_id: current_user.id, skill_id: skill_id)
+      end
+      UserWantSkill.import user_want_skills
     end
-    UserWantSkill.import user_want_skills
     redirect_to new_user_want_ability_path
+  rescue ActiveRecord::RecordInvalid
+    render :new
   end
 
   private
