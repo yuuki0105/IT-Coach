4f5a8ce3357cedf3f845952d6259e2829c0af61a
github yuuki0105 <komatsu3613@gmail.com>
2021-03-31 11:09:11 +0900

    mypageのedit、 updateアクションを削除

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -7,23 +7,4 @@ class MypagesController < ApplicationController
     @coach = @user.coach
   end
 
-  def edit
-    @user = current_user
-    @coach = @user.coach
-  end
-
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      redirect_to mypage_path
-    else
-      render :edit
-    end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :profile, :image,  { user_skills_attributes: [:id, :skill_id, :experience_year, :level]})
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
 
   resource :menu, only: [:show]
 
-  resource :mypage, only: [:show, :edit, :update] do
+  resource :mypage, only: [:show] do
     resource :name, only: [:edit, :update]
     resource :profile, only: [:edit, :update]
     resources :user_skills, only: [:new, :create, :edit, :update, :destroy]
