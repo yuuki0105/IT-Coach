24ee70255a531842c2c849edba9b9479afb81f56
github yuuki0105 <komatsu3613@gmail.com>
2021-04-06 23:20:16 +0900

    google_calendar_tokenのモデルづくり

---
+++ app/models/google_calendar_token.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: google_calendar_tokens
+#
+#  id           :bigint           not null, primary key
+#  coach_id     :bigint           not null
+#  access_token :string(255)      not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_google_calendar_tokens_on_coach_id  (coach_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (coach_id => coaches.id)
+#
+class GoogleCalendarToken < ApplicationRecord
+  belongs_to :coach
+  validates :access_token, presence: true
+end
