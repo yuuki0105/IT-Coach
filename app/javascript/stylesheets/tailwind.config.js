806ff77e916191bee1a9154cb9d66ee1b70344cd
github yuuki0105 <komatsu3613@gmail.com>
2021-04-18 01:08:53 +0900

    fontの登録（Noto Sans JP）

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      'body': ['Noto Sans JP', 'sans-serif']
+     }
   },
   variants: {
     extend: {},
