b002462b3f886560415cc309f6f72d63812fc440
hatsu38 <hajiwata0308@gmail.com>
2021-05-03 18:45:48 +0900

    rubocop --only  Layout/IndentationWidth -A

--- config/application.rb
+++ config/application.rb
@@ -8,22 +8,22 @@ Bundler.require(*Rails.groups)
 
 class ItCoach::Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+  config.load_defaults 6.1
 
     # 生成ファイルの定義
-    config.generators do |g|
-      g.assets     false
-      g.helper     false
-    end
+  config.generators do |g|
+    g.assets     false
+    g.helper     false
+  end
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Tokyo"
-    config.active_record.default_timezone = :local
+  config.time_zone = "Tokyo"
+  config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.default_locale = :ja
+  config.i18n.default_locale = :ja
 end
