3dcba958f6b8492b734cb5de2ac6daebe559fc91
hatsu38 <hajiwata0308@gmail.com>
2021-05-03 18:58:19 +0900

    rubocop --only Layout/EmptyLines -A

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,7 +76,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-
   config.action_mailer.delivery_method = :letter_opener_web
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
