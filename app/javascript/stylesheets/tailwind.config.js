5d71a4c389be1ecfc86042e4aea7bf33710a4af8
github yuuki0105 <komatsu3613@gmail.com>
2021-05-04 13:51:32 +0900

    grayのカラースタイルを登録・修正

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
         sans: ['Noto Sans JP', 'sans-serif']
       },
       colors: {
-         primary: '#6C6AD2',
-         baseGray: '#EDEDED',
-         facebook: '#1877F2',
-         twitter: '#1D9BF0',
-       },
+        primary: '#6C6AD2',
+        backgroundGray: '#EDEDED',
+        blank: '#B4B3B3',
+        borderGray:'#DFDFDF',
+        subtext: '#858484',
+        facebook: '#1877F2',
+        twitter: '#1D9BF0',
+      },
       fontSize: {
         '3.5xl': '2rem',
       },
